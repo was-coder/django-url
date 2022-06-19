0c8534db0bc04c53289656997e71fd9e811438c1
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-06-19 22:13:23 +0100

    feat:hide secret key

--- waslead/waslead/settings.py
+++ waslead/waslead/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4$8+0mjo9-rw&&=%l(m@32yr74gz1m5y2c%%n_dwshb@ng=)*4'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
